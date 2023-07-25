67f302bd7db5b9c301f3433eb4adfa70278a435d
Abi0l9 <cryptog3ms@gmail.com>
2023-07-25 22:56:54 +0100

    icons added

---
+++ src/components/Main/Skills/SkillWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SkillWrapper = () => {
+  return <div className="">SkillWrapper</div>;
+};
+
+export default SkillWrapper;
--- src/components/Main/Skills/index.jsx
+++ src/components/Main/Skills/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SectionWrapper from "../../SectionWrapper";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <SectionWrapper>
+      <div className="border-b-2 md:ml-3 w-[70px]">
+        <p className="text-3xl">Skills</p>
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Skills;

2002b8d71b3330fa205f7ed23fc698c4a2a7641c
Monsur Oyedeji <102850720+Abi0l9@users.noreply.github.com>
2023-08-12 11:55:45 +0100

    Update SkillWrapper.jsx

--- src/components/Main/Skills/SkillWrapper.jsx
+++ src/components/Main/Skills/SkillWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkillWrapper = ({ children }) => {
   return (
-    <div className="flex flex-row mt-8 items-center flex-wrap gap-y-4">
+    <div className="flex flex-row mt-8 items-center gap-x-3 flex-wrap gap-y-4">
       {children}
     </div>
   );
